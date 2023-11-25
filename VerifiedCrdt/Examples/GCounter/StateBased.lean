ea5063048dcf8636b5a55431581ad2ca489ccdec
Mark Lavrentyev <lavrema@outlook.com>
2023-11-25 08:41:53 -0800

    Implement increment op

--- VerifiedCrdt/Examples/GCounter/StateBased.lean
+++ VerifiedCrdt/Examples/GCounter/StateBased.lean
@@ -7,7 +7,7 @@ private structure State (ρ α : Type) where
   replica_counts : ρ → α
 
 /- A GCounter is a grow-only counter i.e. a counter that can only increase but not decrease. -/
-instance GCounter {ρ τ : Type} (α : Type) [Zero α] [Max α] : Crdt ρ τ α where
+instance GCounter {ρ τ : Type} (α : Type) [BEq ρ] [Zero α] [Max α] [Incrementable α] : Crdt ρ τ α where
   state_type := State ρ α
   message_type := ρ → α
   init_state rid := ⟨rid, fun _ => 0⟩
@@ -16,4 +16,4 @@ instance GCounter {ρ τ : Type} (α : Type) [Zero α] [Max α] : Crdt ρ τ α 
   receive state message := ⟨state.my_replica_id, fun rid => max (message rid) (state.replica_counts rid)⟩
   do_op op state _ := match op with
   | GCounterOp.Read => (state, sorry) -- TODO: fix this to sum the values (how to iterate over all ρ values?)
-  | GCounterOp.Increment => (⟨state.my_replica_id, sorry⟩ , ())
+  | GCounterOp.Increment => (⟨state.my_replica_id, fun rid => (if rid == state.my_replica_id then Incrementable.increment else id) (state.replica_counts rid)⟩, ())
